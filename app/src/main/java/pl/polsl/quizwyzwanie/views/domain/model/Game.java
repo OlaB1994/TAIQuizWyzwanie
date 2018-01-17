05e8ab1c1970fad828ab9d77158cd3b5ebc7b6aa
Mateusz <mateusz.pindel@onet.pl>
2018-01-17 20:38:59 +0100

    model dla usera w grze poprawiony model game ładowanie listy gier do MenuFragment

--- app/src/main/java/pl/polsl/quizwyzwanie/views/domain/model/Game.java
+++ app/src/main/java/pl/polsl/quizwyzwanie/views/domain/model/Game.java
@@ -2,36 +2,114 @@ package pl.polsl.quizwyzwanie.views.domain.model;
 
 import android.support.annotation.NonNull;
 
-public class Game implements Comparable<Game> {
+import java.io.Serializable;
+import java.util.List;
+
+public class Game implements Comparable<Game>, Serializable {
 
     public static final int STATE_FINISHED = 0;
     public static final int STATE_WAITING = 1;
     public static final int STATE_YOUR_TURN = 2;
+//
+//    private String opponentUsername;
+//    private int myPoints;
+//    private int opponentPoints;
+//    private int state;
 
-    private String opponentUsername;
-    private int myPoints;
-    private int opponentPoints;
-    private int state;
 
-    public Game(String opponentUsername, int myPoints, int opponentPoints, int state) {
-        this.opponentUsername = opponentUsername;
-        this.myPoints = myPoints;
-        this.opponentPoints = opponentPoints;
-        this.state = state;
+    private String actualCategoryName;
+    private List<Object> categoryRounds;
+    private List<Object> choosenCategories;
+    private String id;
+    private boolean isFinished;
+    private User user1;
+    private User user2;
+    private String whoChoosedCategoryLast;
+    private String whoWinGame;
+
+    public Game() {    }
+
+    public Game(String actualCategoryName, List<Object> categoryRounds, List<Object> choosenCategories, String id, Boolean isFinished, User user1, User user2, String whoChoosedCategoryLast, String whoWinGame) {
+        this.actualCategoryName = actualCategoryName;
+        this.categoryRounds = categoryRounds;
+        this.choosenCategories = choosenCategories;
+        this.id = id;
+        this.isFinished = isFinished;
+        this.user1 = user1;
+        this.user2 = user2;
+        this.whoChoosedCategoryLast = whoChoosedCategoryLast;
+        this.whoWinGame = whoWinGame;
     }
+
+    public String getActualCategoryName() {
+        return actualCategoryName;
+    }
+
+    public List<Object> getCategoryRounds() {
+        return categoryRounds;
+    }
+
+    public List<Object> getChoosenCategories() {
+        return choosenCategories;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean isFinished() {
+        return isFinished;
+    }
+
+    public User getUser1() {
+        return user1;
+    }
+
+    public User getUser2() {
+        return user2;
+    }
+
+    public String getWhoChoosedCategoryLast() {
+        return whoChoosedCategoryLast;
+    }
+
+    public String getWhoWinGame() {
+        return whoWinGame;
+    }
+
+    public Integer getState(){
+        if (this.isFinished) return STATE_FINISHED;
+        else if (this.user1.getMyTurn()) return STATE_YOUR_TURN;
+        else return STATE_WAITING;
+    }
+
+    public String getOpponentUsername(){
+        if (user2.getDisplayName() != null ) {
+            if (!user2.getDisplayName().equals("")) return user2.getDisplayName();
+            else return user2.getEmail();
+        }
+        else return user2.getEmail();
+    }
+
+//    public Game(String opponentUsername, int myPoints, int opponentPoints, int state) {
+//        this.opponentUsername = opponentUsername;
+//        this.myPoints = myPoints;
+//        this.opponentPoints = opponentPoints;
+//        this.state = state;
+//    }
 
     @Override
     public int compareTo(@NonNull Game o) {
-        if (state < o.getState()) return 1;
-        if (state > o.getState()) return -1;
+        if (this.getState() < o.getState()) return 1;
+        if (this.getState() > o.getState()) return -1;
         return 0;
     }
-
-    public int getState() {
-        return state;
-    }
-
-    public String getOpponentUsername() {
-        return opponentUsername;
-    }
+//
+//    public int getState() {
+//        return state;
+//    }
+//
+//    public String getOpponentUsername() {
+//        return opponentUsername;
+//    }
 }
--- app/src/main/java/pl/polsl/quizwyzwanie/views/ui/game/GameFragment.java
+++ app/src/main/java/pl/polsl/quizwyzwanie/views/ui/game/GameFragment.java
@@ -5,11 +5,13 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +19,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import pl.polsl.quizwyzwanie.R;
+import pl.polsl.quizwyzwanie.views.domain.model.Game;
 import pl.polsl.quizwyzwanie.views.domain.model.RoundResult;
 import pl.polsl.quizwyzwanie.views.ui.MainActivity;
 
@@ -25,6 +28,13 @@ import static pl.polsl.quizwyzwanie.views.domain.model.RoundResult.ANSWER_UNDEFI
 import static pl.polsl.quizwyzwanie.views.domain.model.RoundResult.ANSWER_WRONG;
 import static pl.polsl.quizwyzwanie.views.domain.model.RoundResult.DEFAULT_ANSWER;
 import static pl.polsl.quizwyzwanie.views.domain.model.RoundResult.ROUND_PER_GAME;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class GameFragment extends Fragment {
 
@@ -77,5 +87,6 @@ public class GameFragment extends Fragment {
         results.add(new RoundResult());
         return results;
     }
+
 
 }
