69add7a1b818b4d98064cac3fe55ca2dc7ea4b8d
WookiePL <wookiepl@outlook.com>
2018-01-18 19:35:47 +0100

    get Categories list, show 3 Categories on CategoryFragment

--- app/src/main/java/pl/polsl/quizwyzwanie/views/ui/game/CategoryFragment.java
+++ app/src/main/java/pl/polsl/quizwyzwanie/views/ui/game/CategoryFragment.java
@@ -7,10 +7,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import pl.polsl.quizwyzwanie.R;
+import pl.polsl.quizwyzwanie.views.domain.model.Category;
 import pl.polsl.quizwyzwanie.views.ui.MainActivity;
 
 public class CategoryFragment extends Fragment {
@@ -21,6 +32,9 @@ public class CategoryFragment extends Fragment {
     Button secondBtn;
     @BindView(R.id.fragment_category_third_btn)
     Button thirdBtn;
+
+    private List<Category> categoriesList = new ArrayList<>();
+    private List<Category> threeRandomCategoriesList = new ArrayList<>();
 
     @OnClick(R.id.fragment_category_first_btn)
     public void onFirstCategoryClick() {
@@ -62,7 +76,44 @@ public class CategoryFragment extends Fragment {
     }
 
     private void setupView() {
-        //todo setup category names
+        //TODO: extract this to separated thread and refactor
+        DatabaseReference mDatabase;
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        DatabaseReference ref = mDatabase.child("categories");
+        ref.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                    System.out.println("Categories read OK: " + snapshot.getValue());
+                    Category cat = snapshot.getValue(Category.class);
+                    categoriesList.add(cat);
+                    System.out.println("Categories read OK2: " + snapshot.getValue(Category.class));
+                }
+
+                threeRandomCategoriesList = getThreeRandomCategories(categoriesList);
+
+                firstBtn.setText(threeRandomCategoriesList.get(0).getName());
+                secondBtn.setText(threeRandomCategoriesList.get(1).getName());
+                thirdBtn.setText(threeRandomCategoriesList.get(2).getName());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("Categories read failed: " + databaseError.getCode());
+            }
+        });
+    }
+
+    private List<Category> getThreeRandomCategories(List<Category> allCategories) {
+        List<Category> randomList = new ArrayList<>(allCategories);
+        Collections.shuffle(randomList);
+        //TODO: what if there is less than 3 elements in allCategories? :P
+        return randomList.subList(0, 3);
+
     }
 
 }
